da01869222f0404035888f09ba0a51c8bb4ae1ae
Sadman600 <sadmansakib600@gmail.com>
2023-01-31 01:27:12 +0600

    Create a new project with Material and Next.js

---
+++ componants/Footer.js
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+           <Box>
+            <Typography variant='h3'>Footer</Typography>
+            </Box> 
+        </>
+    );
+};
+
+export default Footer;
